98af748ba7b1471a1d374001993aba0f6287231a
photoabcxyz12-coder <photo.abcxyz12@gmail.com>
2025-12-25 14:28:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/photo">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
